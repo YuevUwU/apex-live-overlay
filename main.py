8e5f6b2fb7228a196f7f2e5152c286a8eb15d66d
YuevUwU <96368079+YuevUwU@users.noreply.github.com>
2024-06-10 02:41:50 +0800

    feat: display on web

--- main.py
+++ main.py
@@ -9,25 +9,37 @@ from team_data import custom_team_name
 WEBSOCKET_HOST = "localhost"
 WEBSOCKET_PORT = 7777
 
+with open('templates/index.html', 'r') as f:
+    html_template = f.read()
 
-async def update_team_name(websocket, path):
+def generate_html(rank="", team_name="", score=""):
+    return html_template.replace("{{ rank }}", rank) \
+                        .replace("{{ team_name }}", team_name) \
+                        .replace("{{ score }}", score)
+
+async def modify_html(team_name: str):
+    with open("index.html", 'w') as f:
+        code = generate_html(team_name=team_name)
+        f.write(code)
+
+async def update_team_name(websocket):
     print("Connected!")
 
     async for message in websocket:
-        try:
-            incoming = ObserverSwitched()
-            incoming.ParseFromString(message)
+        # try:
+        incoming = ObserverSwitched()
+        incoming.ParseFromString(message)
 
-            target_player: Player = incoming.target
-            team_id: int = target_player.teamId
-            team_name: str = custom_team_name.get(team_id, "Unknown")
+        target_player: Player = incoming.target
+        team_id: int = target_player.teamId
+        team_name = str(custom_team_name.get(team_id, "Unknown"))
 
-            await websocket.send(team_name)
-            print(f"Observer switched to Team {team_id}: {team_name}")
+        await modify_html(team_name)
+        print(f"Observer switched to Team {team_id}: {team_name}")
 
-        except Exception as e:
-            print(message)
-            print(e)
+        # except Exception as e:
+        #     print(message)
+        #     print(e)
 
 
 async def main():
